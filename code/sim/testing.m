c1a7974e2d9f9e9e6051d73e4fae37a618b8bc7f
mmccraw98 <mmccraw98@gmail.com>
2023-11-30 11:04:41 -0500

    added attraction term

--- code/sim/testing.m
+++ code/sim/testing.m
@@ -1,15 +1,38 @@
 %% start
 clear; close all; clc;
 N = 1800;  % num particles
-pf = 0.5;  % packing fraction
+pf = 0.2;  % packing fraction
 r0 = 2.0;  % ratio of line of sight to excluded volume (bird size)
 v0 = 1.0;  % velocity magnitude
 dt = 1.0;  % timestep
-eta = 0.2;  % noise magnitude
-beta = 0.5;  % excluded volume magnitude
+eta = 0.5;  % noise magnitude
+beta = 0.2;  % excluded volume magnitude
 phi_los = pi;  % line of sight cone
-gamma = 1.0;  % strength of attraction to chimney
-Nsteps = 2000;  % length of run
+gamma = 0.2;  % strength of attraction to chimney
+Nsteps = 1000;  % length of run
 Nplot = 100;  % number of steps to log
 show_plot = true;  % show animation
-[xtotal, ytotal, pols] = vicsek_IW(1800,0.7,2.0,0.2,1.0,0.2,0.5, pi, 1.0, 2000,100,true)
+save_last_N = 500;  % all the simulation data beyond this point will be saved
+[xtotal, ytotal, vxtotal, vytotal, pols, mas] = vicsek_IW(N, pf, r0, v0, dt, eta, beta, phi_los, gamma, Nsteps, Nplot, show_plot, save_last_N);
+
+%%
+clc;
+
+figure (1); hold on;
+plot(pols)
+plot(mas)
+legend('Velocity Order', 'Angular Momentum Order')
+saveas(gcf, 'sim_order_sample.png')
+
+
+
+
+
+
+
+
+
+
+
+
+
--- code/sim/vicsek_IW.m
+++ code/sim/vicsek_IW.m
@@ -1,21 +1,23 @@
-function [xtotal, ytotal, pols] = vicsek_IW(N,pf,r0,v0,dt,eta,beta,phi_los, gamma, Nsteps,Nplot,show_plot)
-%% Test function to loop over vicsek simulation, output trajectories
+function [xtotal, ytotal, vxtotal, vytotal, pols, mas] = vicsek_IW(N, pf, r0, v0, dt, eta, beta, phi_los, gamma, Nsteps, Nplot, show_plot, save_last_N)
 
 % Box Size
 L = 20.0;
 
 % Definte particle sizes (rc) based on packing fraction
-rc = 2.0*L*sqrt(pf/(pi*N));
+rc = 2.0 * L * sqrt(pf / (pi * N));
 
 % scale r0 by rc
-r0 = r0*rc;
+r0 = r0 * rc;
 
 % initialize trajectory values
-xtotal = zeros(N,Nsteps);
-ytotal = zeros(N,Nsteps);
+xtotal = zeros(N, save_last_N);
+ytotal = zeros(N, save_last_N);
+vxtotal = zeros(N, save_last_N);
+vytotal = zeros(N, save_last_N);
 
-% initialize polarization values
-pols = zeros(Nsteps,1);
+% initialize order parameter values
+pols = zeros(Nsteps, 1);
+mas = zeros(Nsteps, 1);
 
 % Initialize Positions and Velocities randomly
 if gamma == 0
@@ -30,17 +32,25 @@ vs = vs .* v0 ./ [vnorm, vnorm];
 % Loop over simulation
 for step = 1:Nsteps
     % Perform Integration
-    vs = vicsekvelocities_IW(N,v0,r0,rc,eta,beta,L,rs,vs, phi_los, gamma) ;
+    vs = vicsekvelocities_IW(N, v0, r0, rc, eta, beta, L, rs, vs, phi_los, gamma) ;
     rs = rs + vs * dt;
     
-    % store polarization
+    % store order parameters
     meanVs = mean(vs);
-    meanVsNrm = sqrt(meanVs(1)^2 + meanVs(2)^2);
-    pols(step) = meanVsNrm/v0;
+    meanVsNrm = sqrt(meanVs(1) ^ 2 + meanVs(2) ^ 2);
+    pols(step) = meanVsNrm / v0;
+    rcm = rs - mean(rs, 1);
+    mas(step) = mean(abs(rcm(:, 1) .* vs(:, 2) / v0 - rcm(:, 2) .* vs(:, 1) / v0) ./ sqrt(sum(rcm' .^ 2))');
     
-    % store trajectories
-    xtotal(:,step) = rs(:,1);
-    ytotal(:,step) = rs(:,2);
+    if step > Nsteps - save_last_N
+        % store trajectories and velocities
+        xtotal(:,step) = rs(:,1);
+        ytotal(:,step) = rs(:,2);
+        vxtotal(:,step) = vs(:,1);
+        vytotal(:,step) = vs(:,2);
+    end
+
+
     
     % Plot cells and velocities
     if mod(step, Nplot) == 0
@@ -54,8 +64,8 @@ for step = 1:Nsteps
             ylim([0 L]);
         else
             quiver(rs(:, 1), rs(:, 2), vs(:, 1), vs(:, 2));
-            xlim([-L L] / 2)
-            ylim([-L L] / 2)
+            xlim([min(-L, min(rs(:, 1))) max(L, max(rs(:, 1)))])
+            ylim([min(-L, min(rs(:, 2))) max(L, max(rs(:, 2)))])
         end
         axis square;
         pause(0.01);
